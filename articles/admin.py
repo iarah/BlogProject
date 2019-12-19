34473a4c773e3a02383b24fb4667fe28f46780dc
iara <ohkernelpanic@gmail.com>
2019-12-19 17:58:17 -0300

    Change in admin.py ArticleAdmin created

--- articles/admin.py
+++ articles/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Article, Comment
 # Register your models here.
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author','created_on')
+    list_filter = ("author",)
+    search_fields = ['title', 'content', 'author']
+ 
+admin.site.register(Article, ArticleAdmin)
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'body', 'post', 'created_on')
